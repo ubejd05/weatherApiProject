5530411ceebbc01a11bae0bb443d9c43afdad00b
Ubejd <ubed.haziri2005@gmail.com>
2021-08-21 19:10:31 +0200

    small change

--- weather.js
+++ weather.js
@@ -8,7 +8,7 @@ class Weather {
 
   // Fetch weather from API
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.units}&appid=${this.apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.units}&appid=${this.apiKey}`);
 
     const responseData = await response.json();
 
